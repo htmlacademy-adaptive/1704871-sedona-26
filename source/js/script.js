b6473814b2c79f3d91c263a540eb3ac4700999a0
Dmoldd <oldtime_tula@mail.ru>
2022-09-03 17:36:03 +0300

    Поработал над навигацией мобильной версии

--- source/js/script.js
+++ source/js/script.js
@@ -1,5 +1,5 @@
 let navMain = document.querySelector('.main-nav');
-let navToggle = document.querySelector('.main-nav__toggle');
+let navToggle = document.querySelector('.main-nav__burger');
 
 navMain.classList.remove('main-nav--nojs');
 
