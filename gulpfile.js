40787571569a4b3d502c662284d938f2fa8dc226
Dmoldd <oldtime_tula@mail.ru>
2022-09-14 21:37:24 +0300

    Сделал perfect pixel всех страниц. Исправил баг с firefox

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,15 @@ import gulp from 'gulp';
 import plumber from 'gulp-plumber';
 import sass from 'gulp-dart-sass';
 import postcss from 'gulp-postcss';
+import csso from 'postcss-csso';
+import rename from 'gulp-rename';
 import autoprefixer from 'autoprefixer';
+// import htmlmin from 'gulp-htmlmin';
+// import terser from 'gulp-terser';
+// import squoosh from 'gulp-libsquoosh';
+// import svgo from 'gulp-svgmin';
+// import svgstore from 'gulp-svgstore';
+// import del from 'del';
 import browser from 'browser-sync';
 
 // Styles
@@ -12,11 +20,56 @@ export const styles = () => {
     .pipe(plumber())
     .pipe(sass().on('error', sass.logError))
     .pipe(postcss([
-      autoprefixer()
+      autoprefixer(),
+      csso()
     ]))
+    .pipe(rename('style.min.css'))
     .pipe(gulp.dest('source/css', { sourcemaps: '.' }))
     .pipe(browser.stream());
 }
+
+// HTML
+
+// export const html = () => {
+//   return gulp.src('source/*.html')
+//   .pipe(htmlmin({ collapseWhitespace: true }))
+//   .pipe(gulp.dest('build'));
+// }
+
+// scripts
+
+// export const script = () => {
+//   return gulp.src('source/js/*.js')
+//   .pipe(terser())
+//   .pipe(gulp.dest('build/js'))
+// }
+
+// Images
+// export const images = () => {
+//   return gulp.src('source/img/**/*.{jpg,jpeg,png}')
+//   .pipe(squoosh({
+//     webp: {}
+//   }))
+//   .pipe(gulp.dest('build/img'))
+// }
+
+// Webp
+
+// export const createWebp = () => {
+//   return gulp.src('source/img/**/*.{jpg,jpeg,png}')
+//   .pipe(squoosh({
+//     webp: {}
+//   }))
+//   .pipe(gulp.dest('build/img'));
+// }
+
+// SVG
+
+// export svg = () => {
+//   gulp.src('source/img/*/**/.svg')
+//   .pipe(svgo())
+//   .pipe(gulp.dest('build/img));
+// }
 
 // Server
 
@@ -41,5 +94,5 @@ const watcher = () => {
 
 
 export default gulp.series(
-  styles, server, watcher
+  styles, server, watcher // html добавить
 );
